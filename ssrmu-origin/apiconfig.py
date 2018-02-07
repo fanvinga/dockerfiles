be0f26f72f842df52176ce2beac6d7306ffc1b2e
FAN VINGA <fanalcest@gmail.com>
2018-02-07 10:23:10 +0800

    ssrmu-origin:add config

--- ssrmu-origin/apiconfig.py
+++ ssrmu-origin/apiconfig.py
@@ -1,13 +1,13 @@
 ﻿# Config
-API_INTERFACE = 'sspanelv2' #mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, glzjinmod, legendsockssr, muapiv2(not support)
-UPDATE_TIME = 60
+API_INTERFACE = 'legendsockssr' #mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, glzjinmod, legendsockssr, muapiv2(not support)
+UPDATE_TIME = 15
 SERVER_PUB_ADDR = '127.0.0.1' # mujson_mgr need this to generate ssr link
 
 #mudb
 MUDB_FILE = 'mudb.json'
 
 # Mysql
-MYSQL_CONFIG = 'usermysql.json'
+MYSQL_CONFIG = 'mysql.json'
 
 # API
 MUAPI_CONFIG = 'usermuapi.json'
